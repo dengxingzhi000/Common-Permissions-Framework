2ee74e9b996c2822f2c54803cfe754f98beaa7c5
awdev <developer@auditoryworks.tech>
2025-10-20 10:09:35 +0800

    示例文件

--- common/core/src/main/java/com/frog/common/exception/GlobalExceptionHandler.java
+++ common/core/src/main/java/com/frog/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.frog.common.exception;
 
 import com.frog.common.response.ApiResponse;
-import com.frog.common.response.ResultCode;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
